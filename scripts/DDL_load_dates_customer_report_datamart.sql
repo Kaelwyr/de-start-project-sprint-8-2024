eb2918ece31c3b21fc8fc83fac804aa453e9d0f5
Elizaveta Kashilova <36787577+Kaelwyr@users.noreply.github.com>
2026-08-12 15:37:03 +0500

    Delete scripts/DDL_load_dates_customer_report_datamart.sql

--- scripts/DDL_load_dates_customer_report_datamart.sql
+++
@@ -1,7 +0,0 @@
--- dwh.load_dates_craftsman_report_datamart определение
-DROP TABLE dwh.load_dates_customer_report_datamart;
-create table dwh.load_dates_customer_report_datamart ( 
-	id bigint generated always as identity not null,
-	load_dttm date not null,
-	constraint load_dates_customer_report_datamart_pk primary key (id)
-);
